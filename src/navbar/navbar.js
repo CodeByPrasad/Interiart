5375178304a730dec6f59dd82759cc46b43cfed9
habeeb <bhamare.k@greytip.com>
2021-10-29 15:54:06 +0530

    first commit

---
+++ src/navbar/navbar.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+function NavBar(props) {
+    return (
+        <div>
+            <Button as="input" type="button" value="Input" />
+            <Button as="input" type="submit" value="Submit" />
+            <Button as="input" type="reset" value="Reset" />
+
+        </div>
+    );
+}
+
+export default NavBar;
